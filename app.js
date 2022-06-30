1930372b2e673bbd4f2a361a41a3b2714d1985ac
javaria qasim <javariaqasim77@gmail.com>
2022-06-30 18:01:18 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,270 @@
+ //  Chapter1
+            // (Alert)
+
+//  Question 1:
+// alert("")
+ 
+//    //  Question 2:
+//    alert("Error! Please enter a valid passward.");
+   
+//    //  Question 3:
+//    alert("Welcome to Js Land...\r\n Happy Coding!")
+
+   
+//    //  Question 4:
+//    alert("Welcome to Js Land...");
+   
+//    //  Question 5:
+//    alert("Happy Coding!\r\n prevent this page from creating additional dialogs");
+   
+//    //  Question 6:
+//    alert("Hello...I can run JS through my web browsers cansole");
+                  
+           //       END
+   //  Chapter2
+           //    (VARIABLES FOR STRINGS)
+   
+   //  Question 1:
+//    var userName = (prompt("username", " username"));
+//    alert("username")
+   
+//    //  Question 2:
+//     var myName = (prompt("myname", " myname"));
+//     alert("myname")
+   
+//     //  Question 3 (a):
+//     var titled = (prompt("titled message"));
+//    document.write = (titled);
+//    alert("titled message")
+   
+
+//     //  Question 3 (b):
+//    var helloworld = ("hello world");
+//    document.getElementById("a").innerHTML = helloworld
+//    //  Question 3 (c):
+//    var helloworld = ("hello world");
+//    alert("hello world");
+   
+//    //  Question 4:
+//    var jhonedoe = ("jhonedoe");
+//    alert("jhonedoe");
+   
+//    var old = ("15 years old");
+//    alert("15 years old");
+   
+ 
+//    alert("certified Mobile Application Development");
+   
+//    // Question 5:
+//    alert("PIZZA\r\n PIZZ\r\n PIZ\r\n PI\r\n P")
+   
+//    //Question 6:
+//    alert("My email address is example@example.com");
+
+
+//    //Question 7:
+//    alert("I am trying to learn from the book A smarter\r\n way to learn javascript");
+   
+//    //Question 8:
+//    var display = ("Yah! I can write HTML content through JavaScript");
+//    document.getElementById("b").innerHTML = display ;
+   
+//    //Question 9:
+//    var display = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//    document.getElementById("c").innerHTML = display ;
+             
+                   //   END 
+   
+   // chapter3   
+           // (VARIABLES FOR NUMBERS)  
+   // Question1
+//    var old = ("15 years old");
+//    document.write(old)
+//    alert("15 years old");  
+   
+//    // Question2
+//    var visited = ("you have visited this site 14 time");
+//    document.write(visited);
+//    alert("you have visited this site 14 time"); 
+   
+//    //Question3
+//    var a = "My birth year is 1990";
+//    var b = "Data type of my ddecleared variable is number"
+//    document.write(a + "<br>" + b +"<br>") 
+   
+    //Question4
+//    var visitor = "jhoendoe";
+//    var titel = "T-shirts";
+//    var quantity = "5";
+//    document.write(visitor + " " + "ordered" + " " + quantity + " " + titel + " " + "on XYZ company" + "<br>")
+                   //   END 
+   // chapter4   
+           // (VARIABLE NAMES: LEGAL & ILLEGAL)  
+   // Question1 
+//    var a = ("Variables name can only contain . numbers, $ and _ . For example :$my_1st variable");
+//    var b = ("Variables must begin with a letter, $ or_, For example : $name,_name or names");
+//    var c = ("Variables name are case sensitive");
+//    var d = ("Variables name should not be JS Keywords");
+//    document.write("<h1>" + "Rules for namingJS variables" + "</h1>" + a + "<br>" + b +"<br>" + c +"<br>" + d +"<br>")
+   
+                    //   END
+   // chapter5 
+           // (MATH EXPRESSIONS)  
+
+   // Question1  
+// var a = "3"
+// var b = "5"
+// var c = "8"
+// document.write("sum of" + "" + a + "and" + "" + b + "is" + c )
+
+   // Question2
+
+
+   // Question3
+//    var a = ("Value after variable decleration is undefined");
+//    var b = ("initial value: 5");
+//    var c = ("Value after increment is: 6");
+//    var d = ("Value after addition is: 13");
+//    var e = ("Value after decrement is: 12");
+//    var f = ("The reminder is : 0")
+//   document.write(a + "<br>" + b +"<br>" + c +"<br>" + d +"<br>" + e +"<br>" + f +"<br>")
+
+  // Question4
+
+// var a = "5"
+// var b = "3000" 
+// document.write("Total cost of buy" + a + "ticket to movie is" + b + "PKR")
+
+
+ // Question5
+              //  table of 4
+//  var table = +prompt("enter the number for multipication table", "enter here")
+//  document.write ("table of" + table + "<br>")
+
+
+//  document.write ( table + "x" + 1 + "=" + table* 1 + "<br>") 
+//  document.write ( table + "x" + 2 +"=" + table* 2 + "<br>") 
+//  document.write ( table + "x" + 3 +"=" + table* 3 + "<br>") 
+//  document.write ( table + "x" + 4 +"=" + table* 4 + "<br>") 
+//  document.write ( table + "x" + 5 +"=" + table* 5 + "<br>") 
+//  document.write ( table + "x" + 6 +"=" + table* 6  + "<br>") 
+//  document.write ( table + "x" + 7 +"=" + table* 7 + "<br>") 
+//  document.write ( table + "x" + 8 +"=" + table* 8 + "<br>") 
+//  document.write ( table + "x" + 9 +"=" + table* 9  + "<br>") 
+//  document.write ( table + "x" + 10 +"=" + table* 10 + "<br>") 
+
+// Question6
+//    var a = ("25°C is 77°F");
+//    var b = ("70°F is 21.11111111111111°C");
+//    document.write(a + "<br>" + b +"<br>")
+
+//  Question7
+//    var a = ("Price of item is 1 is 650");
+//    var b = ("Quantity of item 1 is 3");
+//    var c = ("Price of item 2 is 100");
+//    var d = ("Quantity of item 2 is 7");
+//    var e = ("Shipping charges 100");
+//    var f = ("Total cost of your order is 2750")
+
+//   document.write(a + "<br>" + b +"<br>" + c +"<br>" + d +"<br>" + e +"<br>" + f + "<br>")
+
+
+//  Question8
+// var totalmarks = prompt("Enter your total marks"); 
+// var obtained_marks = prompt("Enter your obtained marks");
+// var percentage = obtained_marks * 100 / 980;
+
+// document.write("Total Marks:" + totalmarks + "<br>"  + "Marks obtained:" +  obtained_marks + "<br>");
+// document.write("Percentage:" + percentage + "<br>");
+
+
+
+//  Question9
+//   var a = ("Total currency in PKR:1748");
+//   document.write(a +"<br>")
+
+//  Question10
+
+
+//  Question11
+// var a = prompt("Enter current year"); 
+// var b = prompt("Enter your birth year");
+// var c = prompt("Enter your age");
+// document.write("current year:" +  a + "<br>" + "Birth year:" + b + "<br>" +"Age:" + c + "<br>")
+
+//  Question11
+
+// var a = prompt("Enter the Radius of circle:")
+// var b = prompt("Enter the circumference is:")
+// var c = prompt("Enter the area")
+// document.write("Radius of circle :" + a + "<br>" + "The circumfrence is:" + b + "<br>" + "The Area:" + c + "<br>")
+
+
+
+
+
+
+
+
+//  Question13 
+// var a = prompt("Favourite snack:")
+// var b = prompt("Current age:")
+// var c = prompt("Estimated Maximum Age:")
+// var d = prompt("Amount of snack per day;")
+// var e = ("you will need 150 choclate chip to last you until the ripe old age of 65")
+// document.write("Favourite snack:" + a + "<br>" + "Current age:" + b + "<br>" + "Estimated Maximum Age:" + c + "<br>" + "Amount of snack per day;" + d + "<br>" + e + "<br>")
+
+
+ 
+
+
+
+ // chapter6   
+           // (MATH EXPRESSIONS) 
+// Question1
+
+// var a = 10
+
+// document.write("<br>" + "<br>" + "Result:" + "<br>")
+// document.write("<br>" + "The value of a is" + a)
+// document.write("<br>" + "------------------" + "<br>")
+// document.write("<br>" + "The value of ++a is:" + (++a))
+// document.write("<br>" + "Now the value of a is" + a + "<br>")
+// document.write("<br>" + "The value of a++ is:" + (a++))   
+// document.write("<br>" + "Now the value of a is" + a + "<br>")
+// document.write("<br>" + "The value of --a is:" + (--a)) 
+// document.write("<br>" + "Now the value of a is" + a + "<br>")
+// document.write("<br>" + "The value of a-- is:" + (a--)) 
+// document.write("<br>" + "Now the value of a is" + a)
+
+
+// Question2
+
+// var result = --a - --b + ++b + b--
+//         //     3 = 1 - 0 + 1 + 1 
+// document.write ("<br>" + "<br>" + "a is :" + a)
+// document.write ("<br>" + "b is:" + b)
+// document.write ("<br>" + "result is:" + result)
+
+
+// Question3
+
+
+// Question5  
+        //   table of 5
+// var table = +prompt("enter the number for multipication table", "enter here")
+//  document.write ("table of" + table + "<br>")
+
+//  document.write ( table + "x" + 1 + "=" + table* 1 + "<br>") 
+//  document.write ( table + "x" + 2 +"=" + table* 2 + "<br>") 
+//  document.write ( table + "x" + 3 +"=" + table* 3 + "<br>") 
+//  document.write ( table + "x" + 4 +"=" + table* 4 + "<br>") 
+//  document.write ( table + "x" + 5 +"=" + table* 5 + "<br>") 
+//  document.write ( table + "x" + 6 +"=" + table* 6  + "<br>") 
+//  document.write ( table + "x" + 7 +"=" + table* 7 + "<br>") 
+//  document.write ( table + "x" + 8 +"=" + table* 8 + "<br>") 
+//  document.write ( table + "x" + 9 +"=" + table* 9  + "<br>") 
+//  document.write ( table + "x" + 10 +"=" + table* 10 + "<br>") 
+
+
+
